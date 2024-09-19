cc93156f4e01067f9f253c25b07fbf2ca9de913d
spudy101 <camilosantanderh@gmail.com>
2024-09-19 16:30:28 -0300

    probando nuevo commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,27 @@
+// const admin = require('firebase-admin');
+// const path = require('path');
+
+// const serviceAccount = require(path.join(__dirname, '../firebase-config.json')); // Asegúrate de que la ruta sea correcta
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   storageBucket: 'women-security-app-31986.appspot.com', // Cambia esto por tu bucket real
+// });
+
+// const db = admin.firestore();
+// const bucket = admin.storage().bucket(); // Ya no necesitas pasar el bucket aquí si está en initializeApp()
+
+// module.exports = { admin, db, bucket };
+
+
 const admin = require('firebase-admin');
-const path = require('path');
 
-const serviceAccount = require(path.join(__dirname, '../firebase-config.json')); // Asegúrate de que la ruta sea correcta
-
+// No necesitas el archivo de credenciales en Cloud Run
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
   storageBucket: 'women-security-app-31986.appspot.com', // Cambia esto por tu bucket real
 });
 
 const db = admin.firestore();
-const bucket = admin.storage().bucket(); // Ya no necesitas pasar el bucket aquí si está en initializeApp()
+const bucket = admin.storage().bucket();
 
 module.exports = { admin, db, bucket };
